10b6ca31b29723896f7485eda64a0f6d003e01b9
Jordan Neal <jordaneal@gmail.com>
2022-05-17 15:49:09 -0700

    Add project files.

---
+++ CrypticCode/Encrypter.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace CrypticCode
+{
+    public class Encrypter
+    {
+        private int _asciiSum;
+        public string Encrypt(string unencrypted)
+        {
+            _asciiSum = 0;
+
+            foreach (char c in unencrypted)
+            {
+                _asciiSum += c;
+            }
+
+            char[] charArr = unencrypted.ToCharArray();
+            int[] intArr = new int[charArr.Length];
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                intArr[i] = (int)(charArr[i] * _asciiSum) - _asciiSum;
+
+                if (i == charArr.Length - 1)
+                {
+                    sb.Append(intArr[i]);
+                    break;
+                }
+                sb.Append($"{intArr[i]},");
+            }
+            return sb.ToString();
+        }
+
+        public string Decrypt(string encrypted)
+        {
+            string[] stringArr = encrypted.Split(',');
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < stringArr.Length; i++)
+            {
+                sb.Append((char)((int.Parse(stringArr[i]) + _asciiSum) / _asciiSum));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CrypticCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using CrypticCode;
+
+Encrypter e = new Encrypter();
+
+string secret = e.Encrypt("THE quiCk BrownfOx jumpS OvEr The Lazy Dog?");
+
+string answer = e.Decrypt(secret);
+
+Console.WriteLine(
+    $"String: THE quiCk BrownfOx jumpS OvEr The Lazy Dog?" +
+    $"\n\nEncrypted: {secret}" +
+    $"\n\nDecrypted: {answer}");
+
+Console.ReadLine();
